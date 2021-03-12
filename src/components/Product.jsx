db5b5267a142a2dbb8412142f747273f4d64f5d0
Ivan-Danilov996 <ivandanilov996@yandex.ru>
2021-03-12 17:21:39 +0300

    Updates

--- src/components/Product.jsx
+++
@@ -1,9 +0,0 @@
-export default function Product({index, title, sizes, price}) {
-    return (
-        <tr>
-            <td>{title}</td>
-            <td>{sizes[0].size}</td>
-            <td>{price} руб.</td>
-        </tr>
-    )
-}
